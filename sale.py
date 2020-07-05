7956504a7dc7124cc02d1b5effbd029b39721b75
raimonesteve <raimonesteve@gmail.com>
2020-07-05 11:10:05 +0200

    Upgrade to 5.6

--- sale.py
+++ sale.py
@@ -4,24 +4,14 @@
 from trytond.pool import Pool, PoolMeta
 from trytond.model import fields
 from trytond.transaction import Transaction
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 __all__ = ['SaleLine']
 
 
 class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
-
-    @classmethod
-    def __setup__(cls):
-        super(SaleLine, cls).__setup__()
-        cls._error_messages.update({
-                'min_qty_product': 'Quantity product line "%s" is less than '
-                    'quantity available for sale in product. '
-                    '(Minimum quantity is: %s).',
-                'max_qty_product': 'Quantity product line "%s" is more than '
-                    'quantity available for sale in product. '
-                    '(Maximum quantity is: %s).',
-                })
 
     @fields.depends('sale')
     def on_change_quantity(self):
@@ -43,11 +33,13 @@ class SaleLine(metaclass=PoolMeta):
                         and self.product and self.product.template.sale_min_qty
                         and self.quantity
                         and not self.product.template.sale_min_qty <= self.quantity):
-                    self.raise_user_error('min_qty_product',
-                        (self.product.rec_name, self.product.sale_min_qty))
+                    raise UserError(gettext('sale_min_qty.msg_min_qty_product',
+                        product=self.product.rec_name,
+                        qty=self.product.sale_min_qty))
                 if (shop.max_qty
                         and self.product and self.product.template.sale_max_qty
                         and self.quantity
                         and not self.product.template.sale_max_qty > self.quantity-1):
-                    self.raise_user_error('max_qty_product',
-                        (self.product.rec_name, self.product.sale_max_qty))
+                    raise UserError(gettext('sale_min_qty.msg_max_qty_product',
+                        product=self.product.rec_name,
+                        qty=self.product.sale_max_qty))
